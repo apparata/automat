7c4e7625d1502adf518235995eebd1173ee870fa
Martin Johannesson <mjohannesson@gmail.com>
2016-10-01 22:45:52 +0200

    First version of the code.

---
+++ StateMachineTemplate.swift
@@ -0,0 +1,95 @@
+import Foundation
+
+/// Example:
+/// ```
+/// let stateMachine = __CLASSNAME__(initialState: __FIRSTSTATE__)
+/// stateMachine.didTransition = { from, event, to in
+///     print("\(from) -\(event)-> \(to)")
+/// }
+/// stateMachine.fire(event: __FIRSTEVENT__)
+/// ```
+public class __CLASSNAME__ {
+    
+    public enum Error: Swift.Error {
+        case illegalTransition(from: State, event: Event)
+    }
+    
+    public enum State {
+        __STATES__
+    }
+    
+    public enum Event {
+        __EVENTS__
+    }
+
+    private typealias Transition = (from: State, event: Event, to: State)
+    
+    public typealias TransitionHandler = (_ from: State, _ event: Event, _ to: State) -> Void
+
+    /// Called right after a transition has been made.
+    public var didTransition: (TransitionHandler)?
+    
+    /// The current state of the state machine.
+    public private(set) var state: State
+    
+    /// Initializes the state machine with an initial state.
+    ///
+    /// - parameter initialState: The initial state of the state machine.
+    public init(initialState: State) {
+        state = initialState
+    }
+    
+    /// Fire an event which may result in a state transition.
+    ///
+    /// - parameter event: Event to fire.
+    /// 
+    /// - returns: `true` if transition was made. `false` otherwise.
+    @discardableResult public func fire(event: Event) -> Bool {
+        guard let transition = __CLASSNAME__.validatedTransition(from: state, event: event) else {
+            return false
+        }
+        make(transition)
+        return true
+    }
+    
+    /// Fire an event which results in either a state transition or a thrown error.
+    ///
+    /// - parameter event: Event to fire.
+    ///
+    /// - throws: `StateMachine.Error` if the transition is illegal.
+    public func fireStrict(event: Event) throws {
+        guard let transition = __CLASSNAME__.validatedTransition(from: state, event: event) else {
+            throw Error.illegalTransition(from: state, event: event)
+        }
+        make(transition)
+    }
+    
+    private static func validatedTransition(from: State, event: Event) -> Transition? {
+        guard let to = transitionDestination(from: from, event: event) else {
+            return nil
+        }
+        return (from: from, event: event, to: to)
+    }
+    
+    private static func transitionDestination(from: State, event: Event) -> State? {
+        switch from {
+        __TRANSITIONS__
+            
+        default:
+            break
+        }
+        
+        return nil
+    }
+    
+    private func make(_ transition: Transition) {
+        state = transition.to
+        didTransition?(transition.from, transition.event, transition.to)
+    }
+}
+
+extension __CLASSNAME__: CustomStringConvertible {
+    public var description: String {
+        return "<__CLASSNAME__(state=\(state))>"
+    }
+}
---
+++ swiftgen.js
@@ -0,0 +1,132 @@
+
+function saveAsSwift() {
+	$("#output").load("StateMachineTemplate.swift", function (template, textStatus, xhr) {
+		var stateMachine = buildStateMachine();
+		if (stateMachine == null) {
+			return;
+		}
+		console.log(links);
+		console.log(stateMachine);
+		var swiftCode = generateSwiftCode(template, stateMachine);
+		document.location.href = 'data:text/plain;base64,' + btoa(swiftCode);
+	});
+}
+
+function generateSwiftCode(template, stateMachine) {
+		
+	var output = template;
+	
+	output = output.replace(/__CLASSNAME__/g, stateMachine.className);
+
+	output = output.replace(/__FIRSTSTATE__/g, stateMachine.states[0].name);
+
+	var statesString = "";
+	for (var i = 0; i < stateMachine.states.length; i++) {
+		statesString += "        case " + stateMachine.states[i].name + "\n";
+	}
+	output = output.replace(/__STATES__/g, statesString.trim());
+
+	output = output.replace(/__FIRSTEVENT__/g, stateMachine.events[0].name);
+
+	var eventsString = "";
+	for (var i = 0; i < stateMachine.events.length; i++) {
+		eventsString += "        case " + stateMachine.events[i].name + "\n";		
+	}
+	output = output.replace(/__EVENTS__/g, eventsString.trim());
+	
+	var transitions = {};
+	for (var i = 0; i < stateMachine.events.length; i++) {
+		var event = stateMachine.events[i];
+		var from = event.from;
+		var to = event.to;
+		var eventName = event.name;
+		
+		if (from in transitions) {
+			transitions[from][eventName] = to;
+		} else {
+			var transitionObject = {}
+			transitionObject[eventName] = to;
+			transitions[from] = transitionObject;
+		}
+	}
+
+	var casesString = "";
+
+	for (from in transitions) {
+		var caseString = ""
+		caseString += "        case ." + from + ":\n";
+	    caseString += "            switch event {\n";
+
+		var events = transitions[from];
+		for (event in events) {
+			var to = events[event];
+			caseString += "            case ." + event + ": return ." + to + "\n";
+		}
+
+		caseString += "            default: break\n";
+		caseString += "            }\n\n";
+		
+		casesString += caseString;
+	}
+	
+	output = output.replace(/__TRANSITIONS__/g, casesString.trim() + "\n");
+	
+	return output;
+}
+
+function buildStateMachine() {
+	
+	var unknownEventCounter = 1
+	
+	var machine = {
+		'className': $("#filename").val().trim(),
+		'states': [],
+		'events': []
+	};
+	for(var i = 0; i < nodes.length; i++) {
+		var node = nodes[i];
+		var stateName = node.text
+		if (stateName == "") {
+			alert("All states must be named.")
+			return null;
+		}
+		var state = {
+			'name': stateName
+		};
+		machine.states.push(state);
+	}
+	for(var i = 0; i < links.length; i++) {
+		var link = links[i];
+		var event = null;
+		var eventName = link.text
+		if (eventName == "") {
+			eventName = "unknownEvent" + unknownEventCounter;
+			unknownEventCounter++;
+		}
+		if(link instanceof SelfLink) {
+			event = {
+				'from': link.node.text,
+				'to': link.node.text,
+				'name': eventName
+			};
+		} else if(link instanceof StartLink) {
+			/*
+			event = {
+				'type': 'StartLink',
+				'node': nodes.indexOf(link.node),
+				'text': link.text,
+			};*/
+		} else if(link instanceof Link) {
+			event = {
+				'from': link.nodeA.text,
+				'to': link.nodeB.text,
+				'name': eventName
+			};
+		}
+		if (event != null) {
+			machine.events.push(event);
+		}
+	}
+		
+	return machine;
+}
